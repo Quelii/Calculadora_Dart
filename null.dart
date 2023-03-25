b29b53520a84dd8050a0a0653a298b9ddcb994f6
Queli Gramacho <112115486+Quelii@users.noreply.github.com>
2023-03-24 21:14:16 -0300

    Add files via upload

---
+++ null.dart
@@ -0,0 +1,13 @@
+void main (){
+
+String media;
+  int n1 = 2;
+  int n2 = 10;
+  int n3 = 6;
+  
+  media = (n1 + n2) /2  >= 6 ? "Ap":"Rp";
+    print(media);
+  
+    String nome;
+      print (nome ?? "nome não informado");
+}
